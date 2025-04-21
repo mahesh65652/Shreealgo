e56dd655ea69a6e717c1227ef7dd724459cb9d2d
Mahesh ramavat <Mramavat52@gmail.com>
2025-04-21 14:41:54 +0530

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -3,6 +3,15 @@ import datetime
 print("Algo system started successfully!")
 print("Current date and time is:", datetime.datetime.now())
 
+import datetime
+
+def main():
+    print("Algo system started successfully!")
+    now = datetime.datetime.now()
+    print("Current date and time is:", now)
+
+if __name__ == "__main__":
+    main()
 
 
 
