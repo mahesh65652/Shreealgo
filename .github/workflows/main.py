5e605eb0b738efc4aef2ec1579e96a41c30f9418
Mahesh ramavat <Mramavat52@gmail.com>
2025-04-21 15:24:30 +0530

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,2 +1,10 @@
-# Main algo trading logic
-print('Algo Mandir Live')
+import datetime
+print("Algo system started successfully!")
+print("Current date and time is:", datetime.datetime.now())
+
+import datetime
+
+def main():
+    print("Algo system started successfully!")
+    now = datetime.datetime.now()
+    print("Current date and time is:", now)
